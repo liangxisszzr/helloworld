c6985136322e43f492bb4889240195fb5d3fb2bc
zuojianhou <981067853@qq.com>
2020-10-13 01:50:16 +0800

    LeetCode Practice

---
+++ src/main/java/com/jumpjob/LeatCode/ReverseLinkedList.java
@@ -0,0 +1,61 @@
+package com.jumpjob.LeatCode;
+
+/**
+ * 翻转链表
+ */
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode test = head;
+        int i = 2;
+        while (i < 6) {
+            head.next = new ListNode(i);
+            head = head.next;
+            i++;
+        }
+        ListNode result1 = solution1(test);
+//        ListNode result2 = solution2(head);
+        while (result1 != null) {
+            System.out.println(result1.val);
+            result1 = result1.next;
+        }
+
+//        while (result2 != null) {
+//            System.out.println(result2.val);
+//            result2 = result2.next;
+//        }
+    }
+
+    /**
+     * 解法一： 迭代法，头插法
+     * @param head
+     * @return
+     */
+    public static ListNode solution1(ListNode head) {
+        ListNode pre = null;
+        ListNode last = head;
+        while (last != null) {
+            //依次跟踪下一个节点
+            ListNode temp = last.next;
+            last.next = pre;
+            pre = last;
+            last = temp;
+        }
+        return pre;
+    }
+
+    /**
+     * 解法二：递归法
+     * @param head
+     * @return
+     */
+    public static ListNode solution2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode res = solution2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
+    }
+}
---
+++ src/main/java/com/jumpjob/LeatCode/SwapNodesInPairs.java
@@ -0,0 +1,80 @@
+package com.jumpjob.LeatCode;
+
+/**
+ * 交换链表中的节点
+ */
+public class SwapNodesInPairs {
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(3);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(4);
+        ListNode res = solution2(head);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+
+    /**
+     * 解法一：
+     *      递归：思路同反转链表相同
+     * @param head
+     * @return
+     */
+    public static ListNode solution1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode temp = solution1(head.next.next);
+        ListNode res = head.next;
+        head.next.next = head;
+        head.next = temp;
+        return res;
+    }
+
+    /**
+     * 解法二：
+     *      迭代法：考虑到除第一对节点无需处理头结点，与后续节点对不形成统一规律，加入哨兵以使规律一致
+     *             后续操作和反转链表类似
+     * @param head
+     * @return
+     */
+    public static ListNode solution2(ListNode head) {
+        ListNode guard = new ListNode(-1);
+        guard.next = head;
+        ListNode first = guard;
+
+        ListNode pre = head;
+        while (pre != null && pre.next != null) {
+            ListNode rear = pre.next;
+
+            //swap
+            pre.next = rear.next;
+            guard.next = rear;
+            rear.next = pre;
+
+            //iteration
+            guard = pre;
+            pre = pre.next;
+        }
+        return first.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
