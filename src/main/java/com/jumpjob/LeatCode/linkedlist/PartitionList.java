7062187f6c70bcf8d1107c852b010d1fc0c6bbd2
zuojianhou <981067853@qq.com>
2020-10-15 23:31:39 +0800

    LeetCode Practice

--- src/main/java/com/jumpjob/LeatCode/linkedlist/PartitionList.java
+++ src/main/java/com/jumpjob/LeatCode/linkedlist/PartitionList.java
@@ -12,7 +12,7 @@ public class PartitionList {
         head.next.next.next = new ListNode(2);
         head.next.next.next.next = new ListNode(5);
         head.next.next.next.next.next = new ListNode(2);
-        ListNode result1 = solution(test, 3);
+        ListNode result1 = solution2(test, 3);
 //        ListNode result2 = solution2(head);
         while (result1 != null) {
             System.out.println(result1.val);
@@ -21,50 +21,44 @@ public class PartitionList {
     }
 
     public static ListNode solution(ListNode head, int x) {
-        ListNode guard = new ListNode(0);
-        guard.next = head;
-        ListNode res = guard;
-        ListNode pre = head;
-        ListNode flag = null;
-        while(head != null) {
-            if(head.val == x) {
-                flag = head;
-                break;
+        ListNode rear = new ListNode(1);
+        ListNode pre = new ListNode(0);
+        ListNode res = pre;
+        ListNode last = rear;
+        while (head != null) {
+            ListNode temp = head.next;
+            if (head.val < x) {
+                pre.next = head;
+                pre = pre.next;
+                pre.next = null;
+            } else {
+                rear.next = head;
+                rear = rear.next;
+                rear.next = null;
+            }
+            head = temp;
+        }
+        pre.next = last.next;
+        return res.next;
+    }
+
+    public static ListNode solution2(ListNode head, int x) {
+        ListNode rear = new ListNode(1);
+        ListNode pre = new ListNode(0);
+        ListNode res = pre;
+        ListNode last = rear;
+        while (head != null) {
+            if (head.val < x) {
+                pre.next = head;
+                pre = pre.next;
+            } else {
+                rear.next = head;
+                rear = rear.next;
             }
             head = head.next;
         }
-        ListNode temp = flag;
-        while(pre != flag) {
-            if(pre.val > x) {
-                ListNode zz = pre;
-                pre = pre.next;
-                guard.next = zz.next;
-                zz.next = temp.next;
-                temp.next = zz;
-                temp = temp.next;
-            } else {
-                guard = guard.next;
-                pre = pre.next;
-            }
-        }
-        while(temp.next != null) {
-            if(temp.next.val < x) {
-                ListNode rear = temp.next;
-                temp.next = rear.next;
-                guard.next = rear;
-                rear.next = flag;
-                guard = guard.next;
-            } else if (temp.next.val == x) {
-                ListNode rear = temp.next;
-                temp.next = rear.next;
-                rear.next = pre.next;
-                pre.next = rear;
-                pre = pre.next;
-                temp = temp.next;
-            } else {
-                temp = temp.next;
-            }
-        }
+        rear.next = null;
+        pre.next = last.next;
         return res.next;
     }
 }
